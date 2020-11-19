177b22c11636d6de09ead1167b8a2a102b8aeca6
Parkjunhu <lgwnsgn@naver.com>
2020-11-20 03:30:31 +0900

    Revert "20.11.20(금) / OX퀴즈(pr리뷰에 따른 내용 수정) / 2번째 Commit & Push"
    This reverts commit 995d99815d3ad3d0c33147cad6219ed6d3eacbbf.

--- oxQuiz/js/oxquiz.js
+++ oxQuiz/js/oxquiz.js
@@ -3,42 +3,42 @@ let questions = [
 
    {
       question : '인간은 동물이다.',
-      answer : 'O',
+      answer : true,
       score : 5
    },
    {
       question : '손흥민은 야구선수다.',
-      answer : 'X',
+      answer : false,
       score : 15
    },
    {
       question : '박지성은 2005년에 PSV에서 맨유로 이적하였다.',
-      answer : 'O',
+      answer : true,
       score : 20
    },
    {
       question : '양배추는 공복혈당을 낮추는데 도움이 되는 채소이다.',
-      answer : 'O',
+      answer : true,
       score : 20
    },
    {
       question : '고구려는 백제 장군 김유신에게 멸망하였다.',
-      answer : 'X',
+      answer : false,
       score : 15
    },
    {
       question : '테란 유닛의 벌처 마인 데미지는 100이다.',
-      answer : 'X',
+      answer : false,
       score : 15
    },
    {
       question : '이영호의 주종은 테란이다.',
-      answer : 'O',
+      answer : true,
       score : 5
    },
    {
       question : '고양이는 그루밍을 하는 동물이다.',
-      answer : 'O',
+      answer : true,
       score : 5
    }
    
@@ -52,50 +52,50 @@ let questions = [
    let totalScore = 0;  // 총점
    let correctCount = 0; // 맞은 개수
    let number = 0; // 문제 번호
-   let checkAnswer = []; // checkAnswer : O,X 버튼 체크 판별 
+   let marking = []; // marking : 오답 판별하는 배열
    
-   answer.addEventListener("click", buttonEventFunc); // O버튼 이벤트리스너 등록
-   wrongAnswer.addEventListener("click", buttonEventFunc); // X버튼 이벤트리스너 등록
+   answer.addEventListener("click", correctAnswerFunc); // O버튼 이벤트리스너 등록
+   wrongAnswer.addEventListener("click", wrongAnswerFunc); // X버튼 이벤트리스너 등록
    $('#question').html(`${number+1}번 문제 : ${questions[number].question} (${questions[number].score}점)`); // 첫 번째 문제 세팅
 
-   
-   // O,X버튼 이벤트 리스너 함수
-   function buttonEventFunc(){
-      if(this.id === 'answer'){  // 사용자가 O버튼을 클릭했다면
-         if(questions[number].answer === 'O'){ // 정답이 O라면
-            alert('정답입니다!');
-            checkAnswer.push('O');  // 사용자가 체크한 O 값을 checkAnswer 배열에 push(추가) = 요소 추가
-            totalScore = totalScore + questions[number].score;
-            correctCount++;
-            number++;
-            showScreen(number);
-   
-         } else{
-            alert('O를 선택하셨으나 정답은 X입니다.');
-            checkAnswer.push('O');
-            number++;
-            showScreen(number);
-         }
-      
+   // O버튼 이벤트리스너 함수
+   function correctAnswerFunc(){
 
-      } else if(this.id === 'wrongAnswer'){ // 사용자가 X버튼을 클릭했다면
-         if(questions[number].answer === 'X'){ // 정답이 X라면
-            alert('정답입니다!');
-            checkAnswer.push('X'); // 사용자가 체크한 X 값을 checkAnswer 배열에 push(추가) = 요소 추가 / Push는 자동으로 인덱스번호를 증가 시켜준다.
-            totalScore = totalScore + questions[number].score;
-            correctCount++;
-            number++;
-            showScreen(number);
-   
-         } else{
-            alert('X를 선택하셨으나 정답은 O입니다.');
-            checkAnswer.push('X');
-            number++;
-            showScreen(number);
-         }
-         
+      if(questions[number].answer === true){
+         alert('정답입니다!');
+         marking.push('정답');
+         totalScore = totalScore + questions[number].score;
+         correctCount++;
+         number++;
+         showScreen(number);
+
+      } else{
+         alert('O를 선택하셨으나 정답은 X입니다.');
+         marking.push('오답');
+         number++;
+         showScreen(number);
       }
+
+   }
+
+   // X버튼 이벤트리스너 함수
+   function wrongAnswerFunc(){
       
+      if(questions[number].answer === false){
+         alert('정답입니다!');
+         marking.push('정답');
+         totalScore = totalScore + questions[number].score;
+         correctCount++;
+         number++;
+         showScreen(number);
+
+      } else{
+         alert('X를 선택하셨으나 정답은 O입니다.');
+         marking.push('오답');
+         number++;
+         showScreen(number);
+      }
+
    }
    
 
@@ -103,13 +103,12 @@ let questions = [
       모든 문제가 끝나면 전체 문제 출력(답까지) */  
    function showScreen(number){
       
-      
       if(number === questions.length){  // 다음문제가 없을 때(전체 문제 출력)
          alert('모든 문제가 끝났습니다!');
          $('#question').empty(); // question에 있는 내용 제거
          $('#selectArea').remove(); // OX 버튼 영역 제거
-         answer.removeEventListener("click", buttonEventFunc); // 이벤트리스너 제거
-         wrongAnswer.removeEventListener("click", buttonEventFunc); // 이벤트리스너 제거
+         answer.removeEventListener("click", correctAnswerFunc); // O버튼 이벤트리스너 제거
+         wrongAnswer.removeEventListener("click", wrongAnswerFunc);  // X버튼 이벤트리스너 제거
          $('#fingerprint p').html('<< 정답 확인 >>');
          $('#question').css({'font-size' : '20px', 'line-height' : '7vh', 'height' : '70vh', 'font-family' : 'Quicksand, sans-serif'}); // 글자크기, 글자간격, 문제영역 설정
          $('#total').css({'display' : 'block', 'background-color' : 'black', 'position' : 'absolute'}); // 총점, 맞은 개수 영역 값
@@ -118,10 +117,14 @@ let questions = [
 
         for(let i = 0; i < questions.length; i++){ // 전체 문제 출력
             
-            if(checkAnswer[i] === questions[i].answer){
-               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) / 정답은 ${questions[i].answer}이고 고른답은 ${checkAnswer[i]} / 맞았습니다.</br>`);
-            } else{
-               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) / 정답은 ${questions[i].answer}이고 고른답은 ${checkAnswer[i]} / 틀렸습니다.</br>`);
+            if(marking[i] === '정답' && questions[i].answer === true){
+               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) 답 : O / 맞았습니다.</br>`);
+            } else if(marking[i] === '정답' && questions[i].answer === false){
+               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) 답 : X / 맞았습니다.</br>`);
+            } else if(marking[i] === '오답' && questions[i].answer === true){
+               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) 답 : O </br>`);
+            } else if(marking[i] === '오답' && questions[i].answer === false){
+               $('#question').append(`${i+1}번 문제 : ${questions[i].question} (${questions[i].score}점) 답 : X </br>`);
             }
             
          }
